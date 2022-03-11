430092a455e49ec98c9332a0e90c1075c8efc087
dinoarpa98 <dinoarpa98@gmail.com>
2022-03-11 17:04:56 +0100

    finish

--- asset/biglietti.js
+++ asset/biglietti.js
@@ -6,24 +6,64 @@
 
 const price = 0.21;
 
-let prezzo = (km * price) ;
 
-let minorenne = document.getElementById("offerta").innerHTML = `Biglietto standard`;
+let nome = document.getElementById("nomeCognome");
+console.log(nome)
 
-let over = document.getElementById("offerta").innerHTML = `Biglietto Over 65`;
+let km = document.getElementById("chilometri");
+console.log(km)
 
-if (minorenne < 18) {
-  let prezzoscontato = prezzo - (prezzo * 20 / 100);
-  console.log(prezzoscontato)
- document.getElementById("offerta").innerHTML = `Biglietto standard`;
-}
+let eta = document.getElementById("selezioneEta");
+console.log(eta)
 
-if (over > 65) {
-  let prezzoanziani = prezzo - (prezzo * 40 / 100) ;
-  console.log(prezzoanziani);
-  document.getElementById("offerta").innerHTML = `Biglietto Over 65`;
-}
+let button = document.getElementById("b-1");
+console.log(button);
 
+let biglietto = document.getElementById("biglietto");
+
+let nomebiglietto = document.getElementById("nomepasseggero");
+console.log(nomebiglietto);
+
+let sconto = document.getElementById("sconto");
+
+let prezzofinale = document.getElementById("prezzofinale");
+
+let randomn1 = Math.floor(Math.random() * 9) + 1;
+
+let randomnumber = Math.floor(Math.random() * 99999) + 1;
+
+let randomcode = document.getElementById("randomnumber")
+
+let randomnmb = document.getElementById("nmb");
+
+let prezzo = (km * price);
+
+button.addEventListener("click",
+
+  function () {
+    console.log(nome.value, km.value, eta.value)
+
+    nomebiglietto.innerHTML = nome.value;
+
+   if(eta.value == "Minorenne"){
+     sconto.innerHTML = 'sconto del : 20%';
+     prezzofinale.innerHTML = `${ (km.value * 0.21) * 0.80} €`
+   } else if (eta.value == "Standard"){
+     sconto.innerHTML = 'Nessuno sconto';
+      prezzofinale.innerHTML = `${ km.value * 0.21} €`
+   } else {
+     sconto.innerHTML = 'sconto del : 40%'
+     prezzofinale.innerHTML = `${ (km.value * 0.21) * 0.60} €`
+   }
+
+   randomcode.innerHTML = randomnumber;
+
+   randomnmb.innerHTML = randomn1;
+   
+
+    biglietto.classList.remove("d-none");
+  }
+)
 
 
 
